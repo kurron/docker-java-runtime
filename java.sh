5766b52f107e690dda3bb84fae0733cc3cada7b9
Ron Kurr <kurr@kurron.org>
2015-12-18 15:44:32 +0000

    It builds and launches

--- java.sh
+++ java.sh
@@ -2,11 +2,12 @@
 
 CMD="docker run \
        --rm \
-       --name maven \
+       --name java \
        --net "host" \
        --user 1000:1000 \
        --volume $(pwd):/pwd \
-       kurron/docker-maven:latest"
+       --volume $HOME:/home/developer \
+       kurron/docker-java:latest"
 
 #echo $CMD
 eval $CMD $*
